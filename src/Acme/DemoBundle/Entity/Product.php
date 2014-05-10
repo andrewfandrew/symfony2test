3ba3adc6ca1dcf2bc1698f821e503888129935b5
andrew <andrew@andrew-VirtualBox>
2014-05-10 14:32:13 +0100

    Created repository for the Product entity in AcmeDemoBundle. The tutorial is here: http://www.youtube.com/watch?v=asC9LL4cHrs.

--- src/Acme/DemoBundle/Entity/Product.php
+++ src/Acme/DemoBundle/Entity/Product.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Product
  *
  * @ORM\Table(name="product")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Acme\DemoBundle\Repository\ProductRepository")
  */
 class Product
 {
@@ -20,7 +20,27 @@ class Product
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
+    
+    /**
+     * @var string $name
+     * 
+     * @ORM\Column(name="name", type="string", length=255, unique=false, nullable=false)
+     */
+    protected $name;
+    
+    /**
+     * @var string $description
+     * 
+     * @ORM\Column(name="description", type="text", unique=false, nullable=true)
+     */
+    protected $description;
+    
+    /**
+     * @var float $price
+     * 
+     * @ORM\Column(name="price", type="decimal", precision=0, scale=2, unique=false, nullable=false)
+     */
+    protected $price;
 
     /**
      * Get id
@@ -30,5 +50,74 @@ class Product
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
     }
 }
